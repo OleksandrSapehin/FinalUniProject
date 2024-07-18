692d032ba02ff97ddb20455209e8a2b72796019c
Oleksandr Sapehin <136280690+OleksandrSapehin@users.noreply.github.com>
2024-07-18 16:16:27 +0200

    questions are saving cleary now

--- src/main/java/org/example/questionsservise/service/impl/VertexAIServiceImpl.java
+++ src/main/java/org/example/questionsservise/service/impl/VertexAIServiceImpl.java
@@ -2,6 +2,8 @@ package org.example.questionsservise.service.impl;
 
 import com.google.cloud.vertexai.api.*;
 import com.google.cloud.vertexai.generativeai.GenerativeModel;
+import com.google.gson.*;
+import com.google.gson.stream.JsonReader;
 import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import org.example.questionsservise.service.VertexAIService;
@@ -9,10 +11,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import java.io.IOException;
+import java.io.StringReader;
 import java.util.*;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-import java.util.stream.Collectors;
 
 
 @Service
@@ -27,10 +27,25 @@ public class VertexAIServiceImpl implements VertexAIService {
         String prompt = "Write in your response only extracted interviewer's questions, comma separated from the following text: " + text;
         GenerateContentResponse response = generativeModel.generateContent(prompt);
         String responseText = response.getCandidatesList().getFirst().getContent().toString();
+        log.info("AI response text: {}", responseText);
 
-        String[] splitText = responseText.split(",");
+        List<String> questions = extractQuestionsFromText(responseText);
 
+        log.info("Extracted questions: {}", questions);
+        return questions;
+    }
+
+    private List<String> extractQuestionsFromText(String text) {
         List<String> questions = new ArrayList<>();
+
+        text = text.replaceAll("role: \"model\"", "")
+                .replaceAll("parts \\{", "")
+                .replaceAll("\\}", "")
+                .replaceAll("text: \"", "")
+                .replaceAll("\"$", "")
+                .trim();
+
+        String[] splitText = text.split("[,\\n]");
 
         for (String part : splitText) {
             part = part.trim();
@@ -38,7 +53,14 @@ public class VertexAIServiceImpl implements VertexAIService {
                 questions.add(part);
             }
         }
-
+        if (!questions.isEmpty()) {
+            int lastIndex = questions.size() - 1;
+            String lastQuestion = questions.get(lastIndex);
+            if (lastQuestion.length() > 3) {
+                questions.set(lastIndex, lastQuestion.substring(0, lastQuestion.length() - 3).trim());
+            }
+        }
+        log.info("Extracted questions from text: {}", questions);
         return questions;
     }
 }
